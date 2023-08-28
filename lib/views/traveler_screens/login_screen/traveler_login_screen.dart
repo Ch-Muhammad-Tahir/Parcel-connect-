ae2fb7e9b60ebfafa6728d968e6632e782d15bd2
Ch Muhammad Tahir <tahir.ctn001@gmail.com>
2023-08-28 19:58:31 +0500

    Code refactor

---
+++ lib/views/traveler_screens/login_screen/traveler_login_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TravelerLoginScreen extends StatelessWidget {
+  const TravelerLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
